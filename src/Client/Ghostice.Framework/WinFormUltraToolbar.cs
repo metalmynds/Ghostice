540e06ff07c5500e7dc5174aaea290eae4a5b1c8
Dave Paterson <crazeydave.2.0@gmail.com>
2016-02-08 09:08:04 +0000

    Updated: Control Framework
             Added Support for Infragistics UltraToolbar.

---
+++ src/Client/Ghostice.Framework/WinFormUltraToolbar.cs
@@ -0,0 +1,34 @@
+﻿using Ghostice.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ghostice.Framework
+{
+    public class WinFormUltraToolbar : WinFormWindowBase
+    {
+
+        public WinFormUltraToolbar(InterfaceControl Parent)
+            : base(Parent)
+        {
+
+        }
+
+        //public Boolean IsToolEnabled(String Key)
+        //{
+
+        //}
+
+        //public Boolean IsToolVisible(String Key)
+        //{
+
+        //}
+
+        public void Press(String Key)
+        {
+            this.HandleResult(GetDispatcher().Perform(ActionRequest.Execute(this.Path, "PerformToolButtonClick", new ActionParameter[] { ActionParameter.Create(Key) })));
+        }
+    }
+
+}
