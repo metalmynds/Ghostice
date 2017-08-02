aff5cf4b609657d327e9ddbf505f911a8c55d3b8
David Paterson <david.paterson@bjss.com>
2017-08-02 04:34:40 +0100

    Added Acceptance Test Project (WIP) Getting error; Severity Code Description Project File Line Suppression State Error CS1029 #error: 'Generation error: Sequence contains no elements' Ghostice.ApplictionKit.Specifications C:\work\giWorking\Ghostice\src\AcceptanceTest\Ghostice.ApplictionKit.Specifications\features\server\ApplicationKitServer.feature.cs 1 Active

---
+++ src/AcceptanceTest/Ghostice.ApplictionKit.Specifications/features/server/ApplicationKitServer.feature
@@ -0,0 +1,17 @@
+﻿Feature: ApplicationKitServer
+	To test a .Net Gui application
+	As an automation tester
+	I need the testing to be cost efficient
+
+@Draft @Integration @Server @
+Scenario Outline: Loading the Server Takes Very Little Time
+	Given I have the <UI> server installed
+	And the endpoint <EndPoint> is clear
+	When I start the <UI> server
+	Then the server should start in under <Timeout> seconds
+
+| UI      | Timeout | EndPoint                |
+| GUI     | 5       | http://localhost:21300/ |
+| Console | 2       | http://localhost:21300/ |
+
+
---
+++ src/AcceptanceTest/Ghostice.ApplictionKit.Specifications/steps/server/ApplicationKitServerSteps.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using TechTalk.SpecFlow;
+
+namespace Ghostice.ApplictionKit.Specifications.steps.server
+{
+    [Binding]
+    public class ApplicationKitServerSteps
+    {
+        private readonly FeatureContext _featureContext;
+        private readonly ScenarioContext _scenarioContext;
+
+        public const String GUI_SERVER_NAME = "WGhostice";
+        public const String CONSOLE_SERVER_NAME = "CGhostice";
+
+
+        public ApplicationKitServerSteps(FeatureContext featureContext, ScenarioContext scenarioContext)
+        {
+            _featureContext = featureContext;
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I have the (.*) server installed")]
+        public void GivenIHaveTheServerInstalled(String ui)
+        {
+            var path = String.Empty;
+
+            if (ui == "Gui")
+            {
+                path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), GUI_SERVER_NAME + ".exe");
+            }
+            else if (ui == "Console")
+            {
+                path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), CONSOLE_SERVER_NAME + ".exe");
+            }
+
+            Assert.IsTrue(File.Exists(path), $"Ghostice Server {ui} at Location {path} Not Found!");
+
+            _scenarioContext.Set<String>(path, "serverPath");
+
+        }
+
+        [Given(@"the endpoint (.*) is clear")]
+        public void GivenTheEndpointIsClear(String endPoint)
+        {
+            var address = new Uri(endPoint);
+
+            using (System.Net.Sockets.TcpClient client = new TcpClient())
+            {
+
+                client.Connect(address.Host, address.Port);
+
+                Assert.Fail($"Unexpected connection to {endPoint} established!");
+
+            }
+
+            _scenarioContext.Set<String>(endPoint, "endPoint");
+
+        }
+
+        [When(@"I start the (.*) server")]
+        public void WhenIStartTheServer(string ui)
+        {
+            ProcessStartInfo serverStartupInfo = new ProcessStartInfo(_scenarioContext.Get<String>("serverPath"));
+
+            serverStartupInfo.Arguments = "-e " + _scenarioContext.Get<String>("endPoint");
+
+            _scenarioContext.Set<DateTime>(DateTime.Now, "start");
+
+            var ghostiseServer = Process.Start(serverStartupInfo);
+
+            ghostiseServer.WaitForInputIdle();
+
+            _scenarioContext.Set<DateTime>(DateTime.Now, "finish");
+
+            ghostiseServer.CloseMainWindow();
+        }
+
+        [Then(@"the server should start in under (.*) seconds")]
+        public void ThenTheServerShouldStartInUnderSeconds(int timeout)
+        {
+            var duration = _scenarioContext.Get<DateTime>("finish") - _scenarioContext.Get<DateTime>("start");
+
+            Assert.IsTrue(duration.Seconds <= timeout, $"Server Failed to Start Promptly! Actual Time: {duration.Seconds}");
+        }
+    }
+}
